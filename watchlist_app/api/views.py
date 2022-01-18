3f54f766a797a55ba677f1463ed02951edb483ad
nepalimohan <nepalimohan@outlook.com>
2022-01-18 13:20:52 +0545

    GET and POST methods implemented in views

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -1,12 +1,29 @@
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from django.shortcuts import render
 from watchlist_app.models import Movie
 from watchlist_app.api.serializers import MovieSerializer
+from django.http import JsonResponse
 
+@api_view(['GET','POST'])
 def movies_list(request):
-    movies = Movie.objects.all()
+    if request.method == "GET":
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data)
+    
+    if request.method == "POST":
+        # movies = Movie.objects.all()
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+            
+
+@api_view()
+def movies_details(request, pk): 
+    movies = Movie.objects.get(pk=pk)
     serializer = MovieSerializer(movies)
     return Response(serializer.data)
-
-def movies_details(request):
-    pass
