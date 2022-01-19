8d144c57128e6abbf5fd6c0c3a13b5b10639b0bc
nepalimohan <nepalimohan@outlook.com>
2022-01-19 17:40:27 +0545

    Changes Made in admin.py file in watchlist_app

--- watchlist_app/admin.py
+++ watchlist_app/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from watchlist_app.models import Watchlist, StreamPlatform
 
-# @admin.register(Watchlist)
-# class WatchllistModelAdmin(admin.ModelAdmin):
-#     list_display = ['id', 'name', 'description','active']
+@admin.register(Watchlist)
+class WatchllistModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'storyline','active', 'created']
+    
+@admin.register(StreamPlatform)
+class StreamPlatformAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'about', 'website']
 
-admin.site.register(Watchlist)
-admin.site.register(StreamPlatform)
+# admin.site.register(Watchlist)
+# admin.site.register(StreamPlatform)
