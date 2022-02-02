06b056ce79f58ba3c7f09f0e62fb5e1c7655e2b7
nepalimohan <nepalimohan@outlook.com>
2022-02-02 18:34:04 +0545

    User_app urls modified

--- watchmate/urls.py
+++ watchmate/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('watch/', include('watchlist_app.api.urls')),
     # path('api-auth', include('rest_framework.urls')),
+    path('account/', include('user_app.api.urls')),
 ]
