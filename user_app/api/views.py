f2294f1b5106a2cf91f3ec8f168939be85841562
nepalimohan <nepalimohan@outlook.com>
2022-02-02 21:43:14 +0545

    serializer.save method overridden

--- user_app/api/views.py
+++ user_app/api/views.py
@@ -1,6 +1,7 @@
+from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
-# from user_app.api.serializers import RegistrationSerializer
+from user_app.api.serializers import RegistrationSerializer
 
 # class RegistrationView(APIView):
 #     def post(self, request):
@@ -12,7 +13,7 @@ from rest_framework.views import APIView
 @api_view(['POST',])
 def registration_view(request):
     if request.method == "POST":
-        serializer = RegistrationSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return serializers.data
+        serializers = RegistrationSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data)
