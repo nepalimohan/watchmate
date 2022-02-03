c1aa69d334830e782bd2f702e04639b28810e023
nepalimohan <nepalimohan@outlook.com>
2022-02-03 09:24:26 +0545

    AutoToken created when login

--- user_app/api/views.py
+++ user_app/api/views.py
@@ -1,7 +1,9 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
+from rest_framework.authtoken.models import Token
 from user_app.api.serializers import RegistrationSerializer
+from user_app import models #for auto token to be created you need to import models.py
 
 # class RegistrationView(APIView):
 #     def post(self, request):
@@ -14,6 +16,20 @@ from user_app.api.serializers import RegistrationSerializer
 def registration_view(request):
     if request.method == "POST":
         serializers = RegistrationSerializer(data=request.data)
+        
+        data = {} #creating own dictionary to store data
+        
         if serializers.is_valid():
-            serializers.save()
-            return Response(serializers.data)
+            account = serializers.save()
+            
+            data['response'] = "Registration Successful!"
+            data['username'] = account.username
+            data['email'] = account.email
+            
+            token = Token.objects.get(user=account).key
+            data['token'] = token
+            
+            
+        else:
+            data =  serializers.errors  
+        return Response(data)
