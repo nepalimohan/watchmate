f60d400150c12e04e35ea87907a5ea44cb928eb7
nepalimohan <nepalimohan@outlook.com>
2022-02-08 21:44:11 +0545

    FurtherChangesMade

--- watchmate/settings.py
+++ watchmate/settings.py
@@ -133,12 +133,12 @@ REST_FRAMEWORK = {
 #     ]
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        # 'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
         # 'rest_framework.authentication.BasicAuthentication',
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        # 'rest_framework_simplejwt.authentication.JWTAuthentication',
      ]
 }
 
-SIMPLE_JWT = {
-    'ROTATE_REFRESH_TOKENS' : True,
-}
+# SIMPLE_JWT = {
+#     'ROTATE_REFRESH_TOKENS' : True,
+# }
