0c6cb02e0d0f362e81a8b2b9382b1514d510c96a
nepalimohan <nepalimohan@outlook.com>
2022-02-08 21:15:56 +0545

    JWT_AccessTOkens_RefreshTok

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -44,7 +44,7 @@ class ReviewCreate(generics.CreateAPIView):
 class ReviewList(generics.ListAPIView):
     # queryset = Reviews.objects.all()
     serializer_class = ReviewSerializer
-    # permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
     
     def get_queryset(self):
         pk = self.kwargs['pk']
