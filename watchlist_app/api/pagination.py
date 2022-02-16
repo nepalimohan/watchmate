b1091e6c2b73a98d5326ab892cc0ade7dbd17a1a
nepalimohan <nepalimohan@outlook.com>
2022-02-16 11:24:48 +0545

    Overriding PaginationMethods

--- watchlist_app/api/pagination.py
+++ watchlist_app/api/pagination.py
@@ -2,3 +2,7 @@ from rest_framework.pagination import PageNumberPagination
 
 class WatchlistPagination(PageNumberPagination):
     page_size = 2
+    # page_query_param = 'p'
+    page_size_query_param = 'size'
+    max_page_size = 5 
+    # last_page_strings = 'end'
